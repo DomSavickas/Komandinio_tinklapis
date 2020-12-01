c01ff4ff51a0c836704aeb9351f42cdcbfb02d01
Ramunas Daukas <ramunas.daukas@gmail.com>
2020-12-01 22:56:14 +0200

    like dislike update

--- index.admin.php
+++ index.admin.php
@@ -8,8 +8,13 @@ include 'assets/img/logo.png';
     <link rel="shortcut icon" type="x-icon" href="images/icon.png" />
     <title>Demo</title>
     <?php require_once ("includes/metadata.php")?>
+    <!--Bootstrap CDN-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
     <script>
             var postsCount = 2;
@@ -30,7 +35,7 @@ include 'assets/img/logo.png';
         }
 
     </script>
-    <script src="scripts.js"></script>
+
 
 </head>
 <body>
@@ -130,6 +135,6 @@ include 'assets/img/logo.png';
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-
+<script src="scripts.js"></script>
 </body>
 </html>
